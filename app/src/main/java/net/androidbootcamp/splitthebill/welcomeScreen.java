228ddeaeb11716ceec295b5c50af441f7072d4d2
Amazing <thienphuoc.0108@gmail.com>
2018-10-16 13:53:43 -0400

    finish

--- app/src/main/java/net/androidbootcamp/splitthebill/welcomeScreen.java
+++ app/src/main/java/net/androidbootcamp/splitthebill/welcomeScreen.java
@@ -1,7 +1,6 @@
 package net.androidbootcamp.splitthebill;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
